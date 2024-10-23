1b99067ccdd2469cacf97da330d6ac2b14578faf
Samoudi_Anas <72927816+SamoudiAnas@users.noreply.github.com>
2024-10-23 22:04:40 +0100

    Add CORS headers to allow cross-origin requests

--- next.config.mjs
+++ next.config.mjs
@@ -24,6 +24,30 @@ const nextConfig = {
 
     return config;
   },
+
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.NEXT_PUBLIC_API_URL,
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
